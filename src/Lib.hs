394faf5d06acdbf2c3bef5cba400aa744292ca64
ŁukaszLampart <ll@ll.ll>
2018-07-15 21:39:10 +0200

    Added basic event configuration

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,6 +14,7 @@ import Control.Concurrent
 import Control.Monad.Trans.Reader  (ReaderT, ask, runReaderT)
 import Control.Monad.IO.Class      (liftIO)
 import Servant
+import Relations
 
 data User = User
   { userId        :: Int
---
+++ src/Relations.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Relations (registerPayment) where
+
+import qualified Data.Map.Strict as Map
+import Control.Concurrent
+import Control.Monad.Trans.Reader  (ReaderT, ask, runReaderT)
+
+data User = User String String deriving (Eq, Show)
+data Money c a = Money { mCur :: c, amount :: a} deriving (Eq, Show)
+
+data PaymentEvent c a = Single
+  { who :: User
+  , currency :: c
+  , payee :: Map.Map User a
+  } deriving (Eq, Show)
+
+type EventLog c a = MVar [PaymentEvent c a]
+
+registerPayment :: Num a => EventLog c a -> PaymentEvent c a -> IO (Money c a)
+registerPayment v (single @ Single {payee=p}) = do
+  events <- takeMVar v
+  putMVar v (single : events)
+  return $ calculate single
+  where calculate :: Num a => PaymentEvent c a -> Money c a
+        calculate Single {currency=cur, payee=p} = Money { mCur=cur, amount=(foldl (\f s -> f + s) 0 p) }
